b7dc5e4a0c35021b332d0352aec8d4a0e619dcf8
deryandreas <derytampubolon@gmail.com>
2025-09-03 13:08:00 +0700

    Complete tutorial 1: Django MVT implementation

--- football_news/settings.py
+++ football_news/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1", "dery-andreas-footballnews.pbp.cs.ui.
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
